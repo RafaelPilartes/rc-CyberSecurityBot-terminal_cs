780b4fdd954be2cc9b077c7ca9a05462443f0106
Rafael Pilartes <167252457+CyberWolfKingg@users.noreply.github.com>
2026-04-12 16:45:18 +0200

    Implemented user identification and personalized greetings

---
+++ CyberSecurityBot/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace CyberSecurityBot.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string PreferredLanguage { get; set; } = "English"; // Future expansion
+    }
+}
---
+++ CyberSecurityBot/Services/ChatbotService.cs
@@ -0,0 +1,46 @@
+﻿using CyberSecurityBot.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CyberSecurityBot.Services
+{
+    public class ChatbotService
+    {
+        private readonly List<dynamic> _responses;
+        private User _currentUser;
+
+        public ChatbotService()
+        {
+            LoadResponses();
+        }
+
+        public void Start()
+        {
+            AudioService.PlayGreetingSound();
+            UiService.ShowHeader();
+
+            InitializeUser();
+        }
+
+        private void InitializeUser()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            _currentUser = new User { Name = !string.IsNullOrWhiteSpace(name) ? name : "Guest" };
+
+            UiService.PrintColoredMessage($"\nWelcome {_currentUser.Name}! Let's learn about cybersecurity.\n", ConsoleColor.Green);
+        }
+
+        private void LoadResponses()
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "responses.json");
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("Could not find responses.json file.", filePath);
+            }
+
+            string jsonContent = File.ReadAllText(filePath);
+        }
+    }
+}
