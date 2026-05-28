a37baa58f8d078f1574516b6579b69ee7e5d133d
Rafael Pilartes <167252457+CyberWolfKingg@users.noreply.github.com>
2026-05-29 01:44:35 +0200

    feat(core): add BotResponses constants including new fallbacks

---
+++ CyberSecurityBot.Core/Constants/BotResponses.cs
@@ -0,0 +1,13 @@
+namespace CyberSecurityBot.Core.Constants
+{
+    public static class BotResponses
+    {
+        public const string GreetingPrompt = "Hello! Welcome to the Cybersecurity Awareness Bot. I'm here to help you stay safe online.";
+        public const string FallbackPrefix = "I'm not sure I understand. You can ask me about: ";
+        public const string FollowupWithoutContext = "Sure, what topic would you like to know more about?";
+        public const string EngineError = "Sorry, I had trouble processing that. Try again.";
+        public const string FarewellMessage = "Stay secure and have a great day!";
+        public const string EmptyInputNotice = "Please type something.";
+        public const string TooLongInputNotice = "Message too long (max 500 characters).";
+    }
+}
